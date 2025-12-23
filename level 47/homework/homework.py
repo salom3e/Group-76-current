7e3ff3ba3dd85678507f0485fb8a38adca51f687
salom3e <salomemaisuradze2010@gmail.com>
2025-12-23 22:07:07 +0400

    GOA

--- level 47/homework/homework.py
+++ level 47/homework/homework.py
@@ -2,10 +2,10 @@
 # რომელიც იწყება დიდი ასოთი, გამოიყენეთ for ციკლი და შესაფერისი სიის და სტრინგის ფუნქციები.
 
 names = ["Meryl", "Julia", "Audrey", "gwen", "marilyn", "gemma", "Monica", "nicole", "gabrielle", "anne", "scarlett"]
-Upper_name = []
+upper_name = []
 
 for name in names:
     if name[0] == name[0].upper():
-        Upper_name.append(name) 
+        upper_name.append(name) 
 
-print(Upper_name)
+print(upper_name)
---
+++ level 51/classwork/classwork.py
@@ -0,0 +1,19 @@
+# ნამუშევარი 
+
+word = "i love arts, music"
+empty = ""
+
+for i in word: 
+    if i not in empty:
+        empty += i
+print(empty)
+
+nums = [2, 4, 5, 7, 6, 8, 10, 26, 2, 5, 2]
+new = []
+
+for i in nums:
+    if i not in new:
+        new.append(i)
+print(new)
+
+# საკალსო დავალება არ გვქონია რადგან ვიმუშავეთ მოსწავლეების ეკრანზე პითონის ამოცანებზე
