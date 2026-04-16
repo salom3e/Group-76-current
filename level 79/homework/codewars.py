449d4f6db3939ba9c1c2569fa7067fd81c6da4ca
salom3e <salomemaisuradze2010@gmail.com>
2026-04-16 16:54:30 +0400

    GOA

--- level 79/homework/codewars.py
+++ level 79/homework/codewars.py
@@ -1,1 +1,22 @@
-# ??
+def multiplication_table(number):
+    
+    result = ""
+    for i in range(1, 11):
+        result += str(i) + " " + "*" + " " + str(number) + " " + "=" + " " + str(i * number)
+        if i != 10:
+            result += "\n"
+    return result
+        
+print(multiplication_table(5))
+
+
+# def multiplication_table(number):
+    
+#     result = ""
+#     for i in range(1, 11):
+#         result += str(i) + " * " + str(number) + " = " + str(i * number)
+#         if i != 10:
+#             result += "\n"
+#     return result
+        
+
